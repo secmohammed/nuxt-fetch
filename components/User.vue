5f965690674584abdaff4c7844d740939f9d075e
mohammed osama <mohammedosama@ieee.org>
2020-05-25 21:03:37 +0200

    initial commit

---
+++ components/User.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ user }}
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            id: {
+                required: true
+            }
+        },
+        data() {
+            return {
+                user: null
+            }
+        },
+        async fetch() {
+            let user = await this.$axios.$get(`https://jsonplaceholder.typicode.com/users/${this.id}`)
+            this.user = user
+        }
+    }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <User id="1" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import User  from '@/components/User'
+export default {
+  components: {
+    User
+  },
+  // data() {
+  //   return {
+  //     user: null
+  //   }
+  // },
+  // async asyncData({ $axios }) {
+  //   let user = await $axios.$get('https://jsonplaceholder.typicode.com/users/1')
+  //   return { user}
+  // }
+}
+</script>
+
+<style>
+</style>
